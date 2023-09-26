c61d3ffcbbd0f34cd07d9b5ff01480a0d65b6c90
DESKTOP-M9FDRN9\csi23-iloposa <csi23-iloposa@colegioaltair.net>
2023-09-26 14:14:41 +0200

    Agregar archivos de proyecto.

---
+++ Servicios/MenuInterfaz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPruebaMenu.Servicios
+{
+   /// <summary>
+   /// Interfaz con la relacion de metodos de la funcionalidad menu
+   /// 250923 - ilp
+   /// </summary>
+    internal interface MenuInterfaz
+    {
+        /// <summary>
+        /// Metodo que muestra mensaje de bienvenida al cajero
+        /// </summary>
+       
+      
+        public void mostrarMensajeBienvenida();
+        
+        /// <summary>
+        /// Metodo que muestra el menu y recoge la selecion del usuario
+        /// 260923 - ilp
+        /// </summary>
+        /// <returns></returns>
+        public int mostrarMenuYSeleccion(); 
+
+    }
+
+}
